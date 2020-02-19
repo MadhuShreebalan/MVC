10fe6b03b825e133934a43a9f54766bd73ff3616
MadhuShreebalan <42761089+MadhuShreebalan@users.noreply.github.com>
2020-02-19 21:25:51 +0530

    Add files via upload

---
+++ Account.Entity/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Account.Entity
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Dob { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public Data(string name, string gender, string email, string dob, string phone, string password)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Dob = dob;
+            this.Email = email;
+            this.Password = password;
+            this.Phone = phone;
+        }
+
+    }
+    
+}
---
+++ Employee.Entity/EmployeeProperty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Employee.Entity
+{
+    public class EmployeeDatabase
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Designation { get; set; }
+
+        public EmployeeDatabase(string name, string id, string designation)
+        {
+            this.Name =name;
+            this.Id = id;
+            this.Designation = designation;
+        }
+    }
+}
