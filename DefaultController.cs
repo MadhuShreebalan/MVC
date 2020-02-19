1943e95cad2a9871d387675d4d7c6e3d3d192c1b
MadhuShreebalan <42761089+MadhuShreebalan@users.noreply.github.com>
2020-02-19 22:26:06 +0530

    Add files via upload

---
+++ DefaultController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Employee.Entity;
+using Employee.repository;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class DefaultController : Controller
+    {
+        EmployeeRepository employeeDatabase;
+        public DefaultController()
+        {
+            employeeDatabase = new EmployeeRepository();
+        }
+        // GET: Default
+        public ActionResult Index()
+        {
+            IEnumerable<EmployeeDatabase> employees = employeeDatabase.GetEmployees();
+            return View(employees);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeDatabase employee)
+        {
+            employeeDatabase.AddEmployee(employee);
+            TempData["Message"] = "Employee Added Successfully!";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            employeeDatabase.DeleteEmployee(id);
+            TempData["Message"] = "Employee Deleted Successfully!";
+            return RedirectToAction("Default");
+        }
+        public ActionResult Edit(string id)
+        {
+            EmployeeDatabase employees = employeeDatabase.GetEmployee(id);
+            return View(employees);
+        }
+    }
+}
